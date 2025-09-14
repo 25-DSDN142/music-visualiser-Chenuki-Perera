dc89a40b4c4c04779c82b19d76c412ee84b9b483
Chenuki-Perera <chenukip@gmail.com>
2025-09-14 22:00:36 +1200

    code

--- music_starter Day one PBA.js
+++ music_starter Day one PBA.js
@@ -1,21 +1,45 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
-let n;
-function setup () {
-createCanvas (windowwidth - 30, windowHeight - 30);
-n = 0;
-frameRate (20) ;
-}
-function draw() {
-translate(width / 2, height / 2);
-rotate(n) ;
-rectSize = width * 0.9;
-background (0); rectMode (CENTER) ;
-for (let i = 1; i < 100; i++) {
-fill(i * 6);
-rect (0, 0, rectSize, rectSize);
-rotate(PI / 30);
-rectSize *= 0.9;
-}
-n += 0.01 ;
-}  
 
+let ballVY = 5;
+let ballSize;
+let myHeight = 800;
+let ballY = myHeight / 2;
+let ballDown = true;
+function setup() {
+//   createCanvas(800, myHeight);
+//   ballY = myHeight / 2;
+//   ballVY = 5;
+  
+}
+
+function draw_one_frame(words, vocal, drum, bass, other, counter) {
+//   console.log(ballY);
+  background(20);
+  textFont('Verdana');66
+  rectMode(CENTER);
+
+  // Map drum volume to bounce speed 
+   let speed = map(drum, 0, 100, 2, 15);
+   ballSize = map(vocal, 0, 100, 2, 250);
+  // Bounce off top and bottom 
+  if (ballY > myHeight - ballSize/2) {
+         ballDown = false
+   }
+  if (ballY < ballSize/2) {
+      ballDown = true;
+  }
+
+    //sets direction of movement
+   if(ballDown){
+      ballY = ballY + speed;
+   }
+   else if(ballDown == false){
+      ballY = ballY - speed;
+   }
+// console.log(vocal);
+
+ // Draw the ball
+  fill(180, 100, 240);
+  noStroke();
+  ellipse(width/2, ballY, ballSize);
+}
